910e7f816bbc00c48d2a72536e367b8598cebca3
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2024-08-13 11:59:18 -0300

    Dart - Variaveis e constantes

---
+++ aula_1/main.dart
@@ -0,0 +1,22 @@
+void main() {
+  /* 
+  Isso é um comentário de
+  varias
+  linhas
+  eu posso escrever várias coisas aqui
+  */
+  // Isso é uma variável
+  String nome = "Anderson";
+  String sobrenome = "Choren";
+  var idade = 32;
+  nome = "João Paulo";
+  idade = 23;
+  print(nome);
+
+  // Constantes
+  const signo = "Virgem";
+  const int anoNascimento = 1991;
+
+  signo = "Gêmeos";
+  anoNascimento = 1980;
+}
