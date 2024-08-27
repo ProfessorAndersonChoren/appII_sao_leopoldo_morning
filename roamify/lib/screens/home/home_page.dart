21d18337dfcde65d470db04ac40f2d8a8e80fef6
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2024-08-27 12:02:13 -0300

    App Roamify - Tela inicial - AppBar

---
+++ roamify/lib/screens/home/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:roamify/screens/home/components/stories.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Roamify'),
+        centerTitle: true,
+        leading: SvgPicture.asset('assets/ic_camera.svg'),
+        leadingWidth: 28,
+        actions: [
+          SvgPicture.asset('assets/ic_directs.svg'),
+        ],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Stories(),
+          ],
+        ),
+      ),
+    );
+  }
+}
